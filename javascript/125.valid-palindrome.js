813eaef76ae201d535225f0e2ff97cb542a186ad
Raus <rausaray1234@gmail.com>
2024-04-09 23:40:41 +0545

    [#125]

---
+++ javascript/125.valid-palindrome.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=125 lang=javascript
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  for (let i = 0; i < s.length / 2; i++) {
+    if (s[i] !== s[s.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+// @lc code=end
