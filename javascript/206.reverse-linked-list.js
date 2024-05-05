1809da977a734b26f5d2d2639c1c774dae2988e2
Raus <rausaray1234@gmail.com>
2024-05-05 10:12:06 +0545

    [#206]

---
+++ javascript/206.reverse-linked-list.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head,prev=null) {
+    if(!head) return prev;
+
+    let next = head.next;
+    head.next =prev;
+
+    return reverseList(next,head) 
+
+};
+// @lc code=end
+
