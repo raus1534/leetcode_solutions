af9a67c6dab6ad003e2bcf31103ca8b8db7f9688
Raus <rausaray1234@gmail.com>
2024-05-08 18:16:36 +0545

    [#21]

---
+++ javascript/21.merge-two-sorted-lists.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=21 lang=javascript
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  let temp = new ListNode();
+  let current = temp;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+  current.next = list1 || list2;
+  return temp.next;
+};
+// @lc code=end
