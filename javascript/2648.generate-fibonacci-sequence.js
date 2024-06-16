18f4eb0d1e85ae9b09a00f0b4db1b906cb2989c9
Raus <rausaray1234@gmail.com>
2024-06-16 17:54:10 +0545

    [#2648]

---
+++ javascript/2648.generate-fibonacci-sequence.js
@@ -0,0 +1,17 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function* () {
+  let n1 = 0,
+    n2 = 1;
+  while (true) {
+    yield n1;
+    [n1, n2] = [n2, n1 + n2];
+  }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
